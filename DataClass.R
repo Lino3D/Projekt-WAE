2a6c472d102925dbaf3115f3f3b1d93b810445c0
Michał <michcio91@gmail.com>
2016-05-28 13:42:56 +0200

    sdf

--- DataClass.R
+++ DataClass.R
@@ -8,3 +8,20 @@ Generate.Particles <- function(nPart=10, nrow = 20, ncol = 20)
   }
   myList
 }
+
+
+CalculateDifference<-function(final, particle)
+{
+  diff = 0
+  for(i in 1:dim(final)[1])  # for each row
+  {
+    for(j in 1:dim(particle)[2]) # for each column
+    {
+      if( final[i,j] != particle[i,j])
+      {
+        diff = diff + 1
+      }
+    }
+  }
+  return(diff)
+}
