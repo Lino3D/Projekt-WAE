a814c107d2f247ad89c97e9a5c716a957ae4c0eb
Lino3D <kamiskijakub@gmail.com>
2016-05-28 15:44:11 +0200

    Init test

--- MainAlgorithm.R
+++ MainAlgorithm.R
@@ -1,4 +1,16 @@
-Main<-function(BetaMap, MinSteps = 15, MaxSteps = 20, ParticlesNumber = 5, dimx = 10, dimy = 10)
+
+InitializeTest<-function(Betamap = gen.board("random", dimx, dimy), ParticlesNumber=10, MinSteps=15, MaxSteps=20, dimx=10, dimy=10)
+{
+  Particles = Generate.Particles(ParticlesNumber,dimx,dimy)
+  
+  Main(Betamap, Particles, ParticlesNumber, MinSteps, MaxSteps , dimx,dimy)
+}
+
+
+
+
+
+Main<-function(BetaMap, Particles, ParticlesNumber, MinSteps = 15, MaxSteps = 20, dimx = 10, dimy = 10)
 {
   if( MinSteps > MaxSteps)
   { 
@@ -11,7 +23,7 @@ Main<-function(BetaMap, MinSteps = 15, MaxSteps = 20, ParticlesNumber = 5, dimx 
     return()
   }
   
-  Particles = Generate.Particles(ParticlesNumber,dimx,dimy)
+
   
   NextStep = Particles
   PreviousStep = Particles
